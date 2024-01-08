a6afc9bbddf02d91f97a2097ce3b91758bc9771a
Alessandro Gasparini <alessandro.gasparini@reddooranalytics.se>
2024-01-08 17:02:48 +0100

    Updated testing with more sensible parameters, looks much better already!

--- testing/01-simdata.R
+++ testing/01-simdata.R
@@ -3,53 +3,49 @@ library(haven)
 library(devtools)
 devtools::load_all()
 set.seed(458622)
-.N <- 25
-.i <- 4 * 25
-.delta <- rep(0.5, 4)
-.beta <- seq(0, 1, length.out = 5)
-.sa <- 1.5
-.sg <- 0.0
-.sp <- 2.5
-.se <- sqrt(10)
-.nu <- 0.0
-.omega <- 1.0
+.k <- 30
+.i <- 100
 
 ### Continuous:
 
 # Gaussian, not informative:
 d0g <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.2, 4),
+  betas = rep(50, 5),
   family = "gaussian",
-  sigma_epsilon = .se,
-  sigma_alpha = .sa,
-  sigma_gamma = .sg,
-  sigma_phi = .sp,
-  nu = .nu
+  sigma_epsilon = sqrt(52.8),
+  sigma_alpha = sqrt(1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(54),
+  nu = -0.2,
+  lambda = exp(-1.5),
+  p = exp(0.0)
 )
 # Gaussian, informative:
 d1g <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.2, 4),
+  betas = rep(50, 5),
   family = "gaussian",
-  sigma_epsilon = .se,
-  sigma_alpha = .sa,
-  sigma_gamma = .sg,
-  sigma_phi = .sp,
-  nu = .nu,
-  omega1 = .omega,
-  omega2 = .omega, # shouldn't matter
-  omega3 = .omega
+  sigma_epsilon = sqrt(52.8),
+  sigma_alpha = sqrt(1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(54),
+  nu = -0.2,
+  lambda = exp(-1.5),
+  p = exp(0.0),
+  omega1 = -0.1,
+  omega2 = 0.0,
+  omega3 = -0.1
 )
 
 ### Binary:
@@ -57,35 +53,39 @@ d1g <- swtrial_inf(
 # Binary, not informative:
 d0b <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.2, 4),
+  betas = rep(-2, 5),
   family = "binomial",
-  sigma_alpha = .sa,
-  sigma_gamma = .sg,
-  sigma_phi = .sp,
-  nu = .nu
+  sigma_alpha = sqrt(0.1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(1.0),
+  nu = -0.1,
+  lambda = exp(-1.5),
+  p = exp(0.0)
 )
 # Binary, informative:
 d1b <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.2, 4),
+  betas = rep(-2, 5),
   family = "binomial",
-  sigma_alpha = .sa,
-  sigma_gamma = .sg,
-  sigma_phi = .sp,
-  nu = .nu,
-  omega1 = .omega,
-  omega2 = .omega, # shouldn't matter
-  omega3 = .omega
+  sigma_alpha = sqrt(0.1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(1.0),
+  nu = -0.1,
+  lambda = exp(-1.5),
+  p = exp(0.0),
+  omega1 = 1.0,
+  omega2 = 0.0,
+  omega3 = 0.4
 )
 
 ### Count:
@@ -93,35 +93,39 @@ d1b <- swtrial_inf(
 # Count, not informative:
 d0c <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.1, 4),
+  betas = rep(-1, 5),
   family = "poisson",
-  sigma_alpha = .sa / 3,
-  sigma_gamma = .sg / 3,
-  sigma_phi = .sp / 3,
-  nu = .nu
+  sigma_alpha = sqrt(0.1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(1.7),
+  nu = -0.2,
+  lambda = exp(-1.5),
+  p = exp(0.0)
 )
 # Count, informative:
 d1c <- swtrial_inf(
   repn = 1,
-  N = .N,
+  k = .k,
   i = .i,
   j = 5,
   intervention_seq = 4,
-  deltas = .delta,
-  betas = .beta,
+  deltas = rep(0.1, 4),
+  betas = rep(-1, 5),
   family = "poisson",
-  sigma_alpha = .sa / 3,
-  sigma_gamma = .sg / 3,
-  sigma_phi = .sp / 3,
-  nu = .nu,
-  omega1 = .omega,
-  omega2 = .omega, # shouldn't matter
-  omega3 = .omega
+  sigma_alpha = sqrt(0.1),
+  sigma_gamma = 0.0,
+  sigma_phi = sqrt(1.7),
+  nu = -0.2,
+  lambda = exp(-1.5),
+  p = exp(0.0),
+  omega1 = 0.8,
+  omega2 = 0.0,
+  omega3 = 0.3
 )
 
 ### Export
--- testing/03-summary.R
+++ testing/03-summary.R
@@ -55,15 +55,79 @@ results <- results |>
   pivot_wider(names_from = "par", values_from = "value") |>
   select(-file) |>
   mutate(i = factor(i, levels = c(0, 1), labels = c("Non Informative", "Informative"))) |>
-  mutate(o = factor(o, levels = c("g", "b", "c"), labels = c("Gaussian", "Binary", "Count"))) |>
   mutate(model = factor(model, levels = c("glmm", "jm"), labels = c("GLMM", "JM")))
-p_res <- ggplot(filter(results, subm == "glmm"), aes(x = term, y = b, color = model)) +
-  geom_errorbar(aes(ymin = b - qnorm(1 - 0.05 / 2) * se, ymax = b + qnorm(1 - 0.05 / 2) * se), position = position_dodge(width = 2 / 3)) +
-  geom_point(position = position_dodge(width = 2 / 3)) +
-  facet_grid(i ~ o, scales = "free") +
-  scale_color_viridis_d(end = 3 / 4) +
-  coord_flip() +
-  theme_bw(base_size = 12, base_family = "DM Sans") +
-  theme(legend.position = "top") +
-  labs(x = "", y = "Parameter Estimate (95% C.I.)", color = "")
-ggsave(filename = here("testing/03-res.pdf"), plot = p_res, device = cairo_pdf, width = 9, height = 7)
+
+# True values
+true <- tribble(
+  ~o, ~subm, ~term, ~true,
+  "g", "glmm", "j=1", 50,
+  "g", "glmm", "j=2", 50,
+  "g", "glmm", "j=3", 50,
+  "g", "glmm", "j=4", 50,
+  "g", "glmm", "j=5", 50,
+  "g", "glmm", "x=1", 0.2,
+  "g", "glmm", "M1[i]", 1.0,
+  "g", "glmm", "M2[i>id]", 1.0,
+  "g", "glmm", "Intercept", 0.0,
+  "g", "glmm", "var(M1[i])", 1.0,
+  "g", "glmm", "var(M2[i>id])", 54,
+  "g", "glmm", "var(e.yobs)", 52.8,
+  "g", "surv", "x=1", -0.2,
+  "g", "surv", "M1[i]", -0.1,
+  "g", "surv", "M2[i>id]", -0.1,
+  "g", "surv", "Intercept", -1.5,
+  "g", "surv", "ln_p", 0.0,
+  "b", "glmm", "j=1", -2,
+  "b", "glmm", "j=2", -2,
+  "b", "glmm", "j=3", -2,
+  "b", "glmm", "j=4", -2,
+  "b", "glmm", "j=5", -2,
+  "b", "glmm", "x=1", 0.2,
+  "b", "glmm", "M1[i]", 1.0,
+  "b", "glmm", "M2[i>id]", 1.0,
+  "b", "glmm", "Intercept", 0.0,
+  "b", "glmm", "var(M1[i])", 0.1,
+  "b", "glmm", "var(M2[i>id])", 1.0,
+  "b", "surv", "x=1", -0.1,
+  "b", "surv", "M1[i]", 1.0,
+  "b", "surv", "M2[i>id]", 0.4,
+  "b", "surv", "Intercept", -1.5,
+  "b", "surv", "ln_p", 0.0,
+  "c", "glmm", "j=1", -1,
+  "c", "glmm", "j=2", -1,
+  "c", "glmm", "j=3", -1,
+  "c", "glmm", "j=4", -1,
+  "c", "glmm", "j=5", -1,
+  "c", "glmm", "x=1", 0.1,
+  "c", "glmm", "M1[i]", 1.0,
+  "c", "glmm", "M2[i>id]", 1.0,
+  "c", "glmm", "Intercept", 0.0,
+  "c", "glmm", "var(M1[i])", 0.1,
+  "c", "glmm", "var(M2[i>id])", 1.7,
+  "c", "surv", "x=1", -0.2,
+  "c", "surv", "M1[i]", 0.8,
+  "c", "surv", "M2[i>id]", 0.3,
+  "c", "surv", "Intercept", -1.5,
+  "c", "surv", "ln_p", 0.0,
+)
+results <- left_join(results, true) |>
+  mutate(o = factor(o, levels = c("g", "b", "c"), labels = c("Gaussian", "Binary", "Count"))) |>
+  mutate(true = ifelse(subm == "surv" & i == "Non Informative" & term %in% c("M1[i]", "M2[i>id]"), 0.0, true)) |>
+  rename(Term = term)
+
+# Plots
+for (.subm in unique(results$subm)) {
+  for (.o in unique(results$o)) {
+    this_plot <- ggplot(filter(results, subm == .subm & o == .o), aes(x = i, y = b, color = model)) +
+      geom_point(aes(y = true), color = "red") +
+      geom_errorbar(aes(ymin = b - qnorm(1 - 0.05 / 2) * se, ymax = b + qnorm(1 - 0.05 / 2) * se), position = position_dodge(width = 2 / 3)) +
+      geom_point(position = position_dodge(width = 2 / 3)) +
+      facet_wrap(~Term, scales = "free_x", labeller = label_both) +
+      scale_color_viridis_d(end = 3 / 4) +
+      coord_flip() +
+      theme_bw(base_size = 12) +
+      theme(legend.position = "top") +
+      labs(x = "", y = "Parameter Estimate (95% C.I.)", color = "", caption = glue("Outcome: {.o}"))
+    ggsave(filename = here(glue("testing/03-res-{.subm}-{.o}.pdf")), plot = this_plot, device = cairo_pdf, width = 8, height = 6)
+  }
+}
